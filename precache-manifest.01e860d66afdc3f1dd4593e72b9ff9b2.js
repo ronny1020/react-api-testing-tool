085186d41640f90accb5bae04280bc55d29de85e
Ronny Hsieh <1020ronny@gmail.com>
2020-06-06 20:32:19 +0800

    Updates

---
+++ precache-manifest.01e860d66afdc3f1dd4593e72b9ff9b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8679fd3e88b788e6dd93667c08be95",
+    "url": "/react-github-api-fetch-demo/index.html"
+  },
+  {
+    "revision": "9ef4dacd1981781605dd",
+    "url": "/react-github-api-fetch-demo/static/css/main.77038df6.chunk.css"
+  },
+  {
+    "revision": "8c83000d0649b8d2f61c",
+    "url": "/react-github-api-fetch-demo/static/js/2.7d2d8225.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-github-api-fetch-demo/static/js/2.7d2d8225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef4dacd1981781605dd",
+    "url": "/react-github-api-fetch-demo/static/js/main.bed7de3b.chunk.js"
+  },
+  {
+    "revision": "40f07ab0bdd2f2ce62aa",
+    "url": "/react-github-api-fetch-demo/static/js/runtime-main.73d06cc8.js"
+  }
+]);
